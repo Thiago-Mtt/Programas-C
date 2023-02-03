0c18753c0eb94f008fc4af5bef5f2ce21b1e375a
Thiago-Mtt <thiagomotta.3000@gmail.com>
2023-02-03 17:37:16 -0300

    Começar implementação de atof em C. Adicionar ignorância de caracteres de espaço no início da entrada da função

---
+++ Funções_stdlib/f_atof.c
@@ -0,0 +1,22 @@
+/*
+Implementação da função atof, da biblioteca stdlib.h
+Recebe um ponteiro pra uma string, com um valor fracionado em ponto flutuante (double)
+Tolera espaços antes do número, suporta sinais(+-), aceita ponto ou virgula
+    como separador entre inteiro e fração
+*/
+
+#include <stdio.h>
+
+double f_atof(char *string_float){
+    //Pular caracteres de espaço no início da entrada
+    while(string_float[0] == ' ' || string_float[0] == '\t'){
+        string_float++;
+    }
+    double numero_float;
+    
+}
+
+int main (){
+    
+    return(0);
+}
