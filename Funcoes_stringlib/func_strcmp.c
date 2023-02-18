1583296df0ae47800db940bba9166b5302b6cc97
Thiago-Mtt <thiagomotta.3000@gmail.com>
2023-02-18 13:36:02 -0300

    Corrigir retorno da função func_strcmp, retorna a diferença de valor entre primeiros caracteres discrepantes entre str1 e str2, ou zero para str1 == str2

--- Funcoes_stringlib/func_strcmp.c
+++ Funcoes_stringlib/func_strcmp.c
@@ -3,24 +3,22 @@
 
 //Compara caracteres até encontrar uma diferença, o fim de uma ou de ambas as strings
 //Em caso do fim de ambas as strings, retorna 0
-//Para uma diferença, com o caratere das string 1 sendo o maior(valor em ASCII), retorna 1
-//Caso contrario, retorna -1
+//Ao encontrar uma diferença, retorna o valor do caractere da str1 subtraido do caractere da str2
 int func_strcmp(const char *str1, const char *str2){
-    for (;;){
-        if(*str1 == '\0' && *str2 == '\0') return 0;
-        else if(*str1 > *str2) return 1;
-        else if(*str1 < *str2) return -1;
+    while(*str1 == *str2 && *str1 != '\0'){
         str1++; str2++;
     }
+    return (int)(*str1 - *str2);
 }
 
 
 int main(){
-    char str1[] = "ABcd";
-    char str2[] = "AbCD";
+    char str1[] = "ABCdef";
+    char str2[] = "ABCf";
     int ret;
 
     ret = func_strcmp(str1, str2);
+    printf("ret = %d\n", ret);
 
     if(ret < 0) {
         printf("str1 is less than str2");
