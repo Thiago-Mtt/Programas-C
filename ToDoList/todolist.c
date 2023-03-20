4459b48c91955dc95b9c7babc0a586cf7b929eb9
Thiago-Mtt <thiagomotta.3000@gmail.com>
2023-03-20 13:14:17 -0300

    Adicionar funções para obter posição do começo da linha X, dada a lista apresentada por último

--- ToDoList/todolist.c
+++ ToDoList/todolist.c
@@ -30,16 +30,16 @@ list uncheck
 #include "todolist.h"
 
 
-//lista_atual mantem registrada qual foi a ultima lista apresentada na tela, a fim de selecionar um item especifico
-//dessa lista
-enum list_state{l_all, l_check, l_uncheck} lista_atual;
+// lista_atual mantem registrada qual foi a ultima lista apresentada na tela
+// A atualização dessa variável deve ser implementada dentro de funções que apresentem uma lista no terminal
+list_state lista_atual;
 
 
 
 
-//Vetor de ponteiros para funções de comandos
+// Vetor de ponteiros para funções de comandos
 #define X(comando, COMANDO) comando ,
-//'COMANDOS_TABLE' termina com um ','
+// 'COMANDOS_TABLE' termina com um ','
 void (*comVect[])(char * param) = {COMANDO_TABLE};
 #undef X
 
@@ -48,7 +48,7 @@ int main() {
     
     //system(CLEAR);
 
-    //Cria a lista caso nao exista, não verifica erro em caso de falha
+    // Cria a lista caso nao exista, não verifica erro em caso de falha
     FILE *fp_lista;
     if(!(fp_lista = fopen( LISTA, "r"))){
         fp_lista = fopen( LISTA, "w");
@@ -56,25 +56,25 @@ int main() {
     }
     fclose(fp_lista);
 
-    //Ler lista completa
+    // Ler lista completa
     ler_lista_all();
 
-    //Loop de comando
+    // Loop de comando
     char entrada[MAX_TAMANHO_COMANDO];
     char comando[MAX_TAMANHO_COMANDO];
     char * param;
     while(1){
-        //Limpar arrays de entrada e comando de lixo e entradas antigas
+        // Limpar arrays de entrada e comando de lixo e entradas antigas
         memset(entrada, '\0', MAX_TAMANHO_COMANDO);
         memset(comando, '\0', MAX_TAMANHO_COMANDO);
 
         printf("\n>");
         fgets(entrada, MAX_TAMANHO_COMANDO, stdin);
-        //Remover caracter de nova linhha no final da entrada
+        // Remover caracter de nova linhha no final da entrada
         if(entrada[strlen(entrada)-1] == '\n') entrada[strlen(entrada)-1] = '\0';
 
-        //param recebe um ponteiro apontando para o primeiro caractere da segunda palavra da entrada (inicio
-        //do parâmetro)
+        // param recebe um ponteiro apontando para o primeiro caractere da segunda palavra da entrada (inicio
+        // do parâmetro)
         param = get_comando(comando, entrada);
 
         en_comando comando_index = str_to_en_comando(comando);
@@ -93,12 +93,12 @@ int main() {
 }
 
 
-//Copia a primeira palavra(o comando) na entrada para o array 'comando'
-//Retorna um ponteiro apontando para a primeira letra da segunda palavra (o(s) parametro(s))
-//Caso nao tenha uma segunda palavra na entrada (o parametro), retorna NULL
+// Copia a primeira palavra(o comando) na entrada para o array 'comando'
+// Retorna um ponteiro apontando para a primeira letra da segunda palavra (o(s) parametro(s))
+// Caso nao tenha uma segunda palavra na entrada (o parametro), retorna NULL
 char * get_comando(char * comando, const char * entrada){
     
-    //Pula espaço em branco no inicio da entrada
+    // Pula espaço em branco no inicio da entrada
     while(*entrada == ' '){
         entrada++;
     }
@@ -106,10 +106,10 @@ char * get_comando(char * comando, const char * entrada){
     int len_comando = strcspn(entrada, " ");
     memcpy(comando, entrada, len_comando);
 
-    //Aponta para o primeiro espaço após a primeira palavra ou para o caracter de fim de string
+    // Aponta para o primeiro espaço após a primeira palavra ou para o caracter de fim de string
     entrada += len_comando;
 
-    //Pula espaço em branco após primeira palavra, agora aponta para início da segunda palavra, se houver
+    // Pula espaço em branco após primeira palavra, agora aponta para início da segunda palavra, se houver
     while(*entrada == ' '){
         entrada++;
     }
@@ -125,8 +125,8 @@ void list(char * param){
 }
 
 
-//Print da lista de tarefas;
-//Tarefas são enumeradas a fim de auxiliar seleção de tarefa
+// Print da lista de tarefas;
+// Tarefas são enumeradas a fim de auxiliar seleção de tarefa
 void ler_lista_all(){
     FILE *fp_lista = fopen(LISTA, "r");
     if(feof(fp_lista)){
@@ -155,10 +155,14 @@ en_comando str_to_en_comando(const char * str){
 #undef X
 
 void add(char * param){
+    // Adiciona tarefa (char * param) ao fim da lista, em uma nova linha.
+    // Tarefa inicialmente não é marcada
     printf("Comando add alcançado\n");
+
     FILE *fp_lista = fopen(LISTA, "a");
-    fputs("\n[ ] ",fp_lista);
+    fputs("[ ] ",fp_lista);
     fputs(param, fp_lista);
+    fputs("\n",fp_lista);
     fclose(fp_lista);
 
     ler_lista_all();
@@ -171,12 +175,112 @@ void del(char * param){
     return;
 }
 
-void check(char * param){
+void check(char * param)
+{
+    // Marca tarefa como cumprida, adicionando um 'X' entre as chaves '[ ]' à esquerda da tarefa selecionada
+    // A tarefa é selecionada a partir da numeração da última lista apresentada (completa, só marcadas, só não marcadas, etc)
+    // 'param' pode ser o número da linha a ser marcada ou 'all' para marcar todas as linhas da última lista apresentada
+    
     printf("Comando check alcançado\n");
+
+    FILE * fp_lista = fopen(LISTA, "r");
+    char str[MAX_TAMANHO_TAREFA];
+    fpos_t linha_pos = get_tarefa(atoi(param), lista_atual);
+
+    fsetpos(fp_lista, &linha_pos);
+    fgets(str, MAX_TAMANHO_TAREFA, fp_lista);
+    printf("Linha selecionada = %s\n", str);
+
+
+
+
     return;
 }
 
 void uncheck(char * param){
     printf("Comando uncheck alcançado\n");
     return;
+}
+
+fpos_t get_tarefa(int linha, list_state lista_atual){
+    
+    switch (lista_atual)
+    {
+        case l_all : 
+            return get_tarefa_l_all(linha);
+            break;
+        case l_check :
+            return get_tarefa_l_check(linha);
+            break;
+        case l_uncheck :
+            return get_tarefa_l_uncheck(linha);
+            break;
+    }
+
+}
+
+fpos_t get_tarefa_l_all(int linha)
+{
+
+    printf("get_tarefa_l_all alcançado\n");
+
+    FILE * fp_lista = fopen(LISTA, "r");
+    int count_linha = 1;
+    char read_char;
+    fpos_t pos_linha;
+
+    while(count_linha != linha || feof(fp_lista))
+    {
+        read_char = fgetc(fp_lista);
+        printf("Caracter lido = %c\n", read_char);
+        if (read_char == '\n' && !feof(fp_lista)) count_linha++; 
+    }
+
+    if (feof(fp_lista)) return -1;
+
+    fgetpos(fp_lista, &pos_linha);
+    return pos_linha;
+}
+
+fpos_t get_tarefa_l_check(int linha)
+{
+    FILE * fp_lista = fopen(LISTA, "r");
+    int count_linha = 0;
+    char read_char;
+
+    while(count_linha != linha || feof(fp_lista))
+    {
+        read_char = fgetc(fp_lista);
+        if (read_char == '\n' && !feof(fp_lista))
+        {
+            fseek(fp_lista, 1, SEEK_CUR);
+            if(fgetc(fp_lista) == CHECKMARK) count_linha++;
+        }
+    }
+
+    if (feof(fp_lista)) return -1;
+
+    return count_linha;
+}
+
+fpos_t get_tarefa_l_uncheck(int linha)
+{
+    FILE * fp_lista = fopen(LISTA, "r");
+    int count_linha = 0;
+    char read_char;
+
+    while(count_linha != linha || feof(fp_lista))
+    {
+        read_char = fgetc(fp_lista);
+        if (read_char == '\n' && !feof(fp_lista))
+        {
+            fseek(fp_lista, 1, SEEK_CUR);
+            if(fgetc(fp_lista) == CHECKMARK) count_linha++;
+        }
+    }
+
+    if (feof(fp_lista)) return -1;
+
+    return count_linha;
+
 }
--- ToDoList/todolist.h
+++ ToDoList/todolist.h
@@ -1,49 +1,62 @@
 #ifndef todolist_HEADER
 #define todolist_HEADER
 
-#define CLEAR "cls"             //Comando do sistema operacional para limpar o prompt de comando
-#define MAX_TAMANHO_TAREFA 30   //Numero máximo de caracteres em uma linha na lista de tarefas 
-#define MAX_TAMANHO_COMANDO 40  //Máximo de caracteres recebidos como entrada
-#define LISTA "lista.txt"       //Nome do arquivo onde se implementa a lista
+#define CLEAR "cls"             // Comando do sistema operacional para limpar o prompt de comando
+#define MAX_TAMANHO_TAREFA 30   // Numero máximo de caracteres em uma linha na lista de tarefas 
+#define MAX_TAMANHO_COMANDO 40  // Máximo de caracteres recebidos como entrada
+#define LISTA "lista.txt"       // Nome do arquivo onde se implementa a lista
+#define CHECKMARK 'X'           // Caracter utilizado para marcar tarefa como cumprida
+#define UNCHECKMARK ' '           // Caracter utilizado para marcar tarefa como não cumprida
+#define PARAM_ALL "all"         // Parametro passado com comandos para indicar seleção de todos os itens da lista
 
-//X Macro. Guarda os comandos reconhecidos pelo programa, utilizada para automatizar declaração das funções, codificação
+// Enumeração utilizada para codificar qual lista foi apresentada ao usuário por último
+typedef enum {l_all, l_check, l_uncheck} list_state;
+
+// X Macro. Guarda os comandos reconhecidos pelo programa, utilizada para automatizar declaração das funções, codificação
 //  dos comandos e facilitar adição de um novo comando
-//A definição de X(comando,COMANDO) indica aplicação desta macro.
-//[comando] escrito em minusculo para funções, maiusculo para enum
-//Adicionar um comando nessa lista requer também a definição de sua função, a qual é declarada automaticamente
-#define COMANDO_TABLE \
-    X(list, LIST)\          //Listar tarefas com ou sem filtros (Ex: mostrar só tarefas não marcadas)
-    X(add, ADD)\            //Adicionar tarefa
-    X(del, DEL)\            //Deletar tarefa(s) (Ex: deletar a tarefa selecionada ou deletar todas as marcadas)
-    X(check, CHECK)\        //Marcar tarefa(s) como cumpridas
-    X(uncheck, UNCHECK)     //Desmarcar tarefa(s) como cumpridas
+// A definição de X(comando,COMANDO) indica aplicação desta macro.
+// [comando] escrito em minusculo para funções, maiusculo para enum
+// Adicionar um comando nessa lista requer também a definição de sua função, a qual é declarada automaticamente
+#define COMANDO_TABLE\
+    X(list, LIST)\
+    X(add, ADD)\
+    X(del, DEL)\
+    X(check, CHECK)\
+    X(uncheck, UNCHECK)
 
-
-//Declaração das funções dos respectivos comandos
+// Declaração das funções dos respectivos comandos
 #define X(comando, COMANDO) void comando(char * param);
 COMANDO_TABLE
 #undef X
 
-//Definição do tipo en_comando, codificação dos comandos
+// Definição do tipo en_comando, codificação dos comandos
 #define X(comando, COMANDO) COMANDO ,
 typedef enum {
     COMANDO_TABLE
     NRO_COMANDOS, EN_ERR=-1
 } en_comando;
-#undef X
+# undef X
 
-//Retorna o valor codificado do comando passado como string
+// Retorna o valor codificado do comando passado como string
 en_comando str_to_en_comando(const char * str);
 
-//Copia a primeira palavra(o comando) da entrada para o array 'comando'
-//Retorna um ponteiro apontando para a primeira letra da segunda palavra (o(s) parametro(s))
-//Caso nao tenha parametro, retorna NULL
+// Copia a primeira palavra(o comando) da entrada para o array 'comando'
+// Retorna um ponteiro apontando para a primeira letra da segunda palavra (o(s) parametro(s))
+// Caso nao tenha parametro, retorna NULL
 char * get_comando(char * comando, const char * entrada);
 
-//Abre arquivo LISTA e emite no terminal as tarefas em cada linha
-//Tarefas são enumeradas a fim de auxiliar seleção de tarefa para operaçãos como 'check' e 'del'
+// Abre arquivo LISTA e emite no terminal as tarefas em cada linha
+// Tarefas são enumeradas a fim de auxiliar seleção de tarefa para operaçãos como 'check' e 'del'
 void ler_lista_all();
 
+// Retorna a posição do início da linha requerida pelo usuário mediante o parâmetro 'linha'
+// A linha requerida depende da lista sendo apresentada
+// Caso ocorra uma falha (Ex: o número passado é 8 mas a lista só tem 6 linhas), retorna -1
+fpos_t get_tarefa           (int linha, list_state lista_atual);
 
+// Redirecionamento da função get_tarefa após identificar a lista atual
+fpos_t get_tarefa_l_all     (int linha);
+fpos_t get_tarefa_l_check   (int linha);
+fpos_t get_tarefa_l_uncheck (int linha);
 
 #endif //todolist_HEADER
